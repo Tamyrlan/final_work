4448705d00b3cd9cc7b4e023dda4bb4adcc88f57
Tamyrlan <tshurenov@gmail.com>
2022-09-19 16:58:37 +0600

    new method with rnd

--- final_work/Program.cs
+++ final_work/Program.cs
@@ -3,7 +3,7 @@ int size = Convert.ToInt32(Console.ReadLine());
 string[] arrstring = new string[size];
 for (int i = 0; i < size; i++)
 {
-    Console.WriteLine($"Enter {i+1} element of array pls:");
+    Console.WriteLine($"Enter {i + 1} element of array pls:");
     arrstring[i] = Console.ReadLine();
 }
 void PrintArr(string[] array)
@@ -16,30 +16,21 @@ void PrintArr(string[] array)
     Console.Write(array[array.Length - 1]);
     Console.Write("]");
 }
-string[] ResizeArray(string[] array)
+string[] FillNewArr(string[] array, int min, int max)
 {
-    int length = array.Length;
-    string[] tempArray = new string[length];
-    int j = 0;
-    for (int i = 0; i < array.Length; i++)
+    Random rnd = new Random();
+    int newSize = rnd.Next(min, max);
+    string[] newArr = new string[newSize];
+    for (int i = 0; i < newSize; i++)
     {
-        if (array[i].Length <= 3)
-        {
-            tempArray[j] = array[i];
-            j++;
-        }
+        newArr[i] = array[i];
     }
-    string[] resArray = new string[j];
-
-    for (int i = 0; i < j; i++)
-    {
-        resArray[i] = tempArray[i];
-    }
-    return resArray;
+    return newArr;
 }
+
 Console.Write("Current array =>");
 PrintArr(arrstring);
-string[] res = ResizeArray(arrstring);
+string [] newArray = FillNewArr(arrstring, 1, 3);
 Console.WriteLine();
 Console.Write("new array =>");
-PrintArr(res);
+PrintArr(newArray);
